5dd18a278c66d520a6684137852b9e686120ca13
Thanaphoom ArunChit <67995808+thanaphoomkmutnb@users.noreply.github.com>
2020-07-15 13:45:13 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name, lastname;
+    int salary, sales, commission;
+    cout << "****HomeWork 2***********\n";
+    cout << "Enter Name               : ";
+    cin >> name >> lastname;
+    cout << "Enter Salary             : ";
+    cin >> salary;
+    cout << "Enter Sale               : ";
+    cin >> sales;
+    cout << "Enter Commission Percent : ";
+    cin >> commission;
+    cout << "Your Name = " << name << " " << lastname << endl;
+    cout << "Total Revenue \t" << (sales*commission/100)+salary << endl;
+	if (cin.fail()) {
+		cout << "Input Error \n";
+	}
+    system("pause");
+    return 0;
+}
